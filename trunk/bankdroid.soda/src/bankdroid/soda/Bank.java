ef7826ac88809a95a3faedfd0f53275ff919d3eb
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2009-09-08 19:38:23 +0000

    pattern matching fixed

--- trunk/bankdroid.soda/src/bankdroid/soda/Bank.java
+++ trunk/bankdroid.soda/src/bankdroid/soda/Bank.java
@@ -1,37 +1,96 @@
 package bankdroid.soda;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author user
+ * 
+ * TODO currently the bank list is hard coded but it should come from a configuration file.
+ * TODO phone number matching should rely on the number endings.
+ *
+ */
 public class Bank
 {
 	public static Bank[] getAvailableBanks()
-	{//TODO currently the bank list is hard coded but it should come from a configuration file.
+	{//
 		final Bank[] banks = new Bank[] { //
-		new Bank("OTP Bank", 1800, "+36201000000", "OTP .*: ([a-zA-Z0-9]*) .*", R.drawable.otp_logo),
-				new Bank("K&H Bank", 1800, "+36202000000", "KHB .*: ([a-zA-Z0-9]*) .*", R.drawable.khb_logo), };
+		new Bank("OTP Bank", 1800, new String[] { "+36201000000" }, //
+				new String[] { "OTP .*: ([a-zA-Z0-9]*) .*" }, R.drawable.otp_logo),
+
+		new Bank("K&H Bank", 1800, new String[] { "+36202000000" }, //
+				new String[] { "KHB .*: ([a-zA-Z0-9]*) .*" }, R.drawable.khb_logo),
+
+		new Bank("ERSTE Bank", 1800, new String[] { "+36303444481" }, //
+				new String[] { ".* ERSTE NetBank [^:]*: ([0-9]*)" }, R.drawable.khb_logo), //FIXME Logo, validity
+
+		};
 		return banks;
 	}
 	private final String id;
+
 	/**
 	 * Validity period of an SMS OTP expressed in seconds.
 	 */
 	private final int otpValidityPeriod;
-	private final String phoneNumber;
-	private final String extractExpression;
-	private final Pattern extractPattern;
+	/**
+	 * One or more phone number can be registered to the Bank.
+	 */
+	private final String[] phoneNumbers;
+	private final String[] extractExpressions;
+	/**
+	 * Contains compiled regular expressions. Initialized in lazy mode, as most of the banks will be never used on
+	 * the same mobile. 
+	 */
+	private Pattern[] patterns;
 
 	private final int iconId;
 
-	public Bank( final String id, final int otpValidityPeriod, final String phoneNumber,
-			final String extractExpression, final int iconId )
+	public Bank( final String id, final int otpValidityPeriod, final String[] phoneNumber,
+			final String[] extractExpression, final int iconId )
 	{
 		super();
 		this.id = id;
 		this.otpValidityPeriod = otpValidityPeriod;
-		this.phoneNumber = phoneNumber;
-		this.extractExpression = extractExpression;
+		this.phoneNumbers = phoneNumber;
+		this.extractExpressions = extractExpression;
 		this.iconId = iconId;
-		extractPattern = Pattern.compile(extractExpression);
+	}
+
+	public boolean isBankPhoneNumber( final String phoneNumber )
+	{
+		for ( int i = 0; i < phoneNumbers.length; i++ )
+		{
+			final String pn = phoneNumbers[i];
+
+			if ( pn.equals(phoneNumber) )
+				return true;
+		}
+		return false;
+	}
+
+	private void initPatterns()
+	{
+		if ( patterns == null )
+		{
+			patterns = new Pattern[extractExpressions.length];
+			for ( int i = 0; i < patterns.length; i++ )
+			{
+				patterns[i] = Pattern.compile(extractExpressions[i]);
+			}
+		}
+	}
+
+	public String getCode( final String message )
+	{
+		initPatterns();
+		for ( int i = 0; i < patterns.length; i++ )
+		{
+			final Matcher matcher = patterns[i].matcher(message);
+			if ( matcher.find() )
+				return matcher.group(1);
+		}
+		return null;
 	}
 
 	public String getId()
@@ -44,19 +103,14 @@ public class Bank
 		return otpValidityPeriod;
 	}
 
-	public String getPhoneNumber()
+	public String[] getPhoneNumbers()
 	{
-		return phoneNumber;
+		return phoneNumbers;
 	}
 
-	public String getExtractExpression()
+	public String[] getExtractExpression()
 	{
-		return extractExpression;
-	}
-
-	public Pattern getExtractPattern()
-	{
-		return extractPattern;
+		return extractExpressions;
 	}
 
 	public int getIconId()
