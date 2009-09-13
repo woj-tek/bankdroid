58864149332a7d022e744520b426d3ad4cbf489e
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2009-09-13 08:03:53 +0000

    Menu and preferences handling added

---
+++ bankdroid.soda/src/bankdroid/soda/Codes.java
@@ -0,0 +1,17 @@
+package bankdroid.soda;
+
+public interface Codes
+{
+	public static final String BANKDROID_SODA_SMSMESSAGE = "bankdroid.soda.SMSMessage";
+	public static final String BANKDROID_SODA_SMSTIMESTAMP = "bankdroid.soda.SMSTimestamp";
+
+	public static final String BANKDROID_SODA_BANK = "bankdroid.soda.Bank";
+
+	public static final String BANKDROID_SODA_SMSCODE = "bankdroid.soda.SMSCode";
+
+	public static final String PREF = "bankdroid.soda";
+	public static final String PREF_NOTIFICATION = "bankdroid.soda.Notification";
+	public static final String PREF_KEEP_SMS = "bankdroid.soda.KeepSMS";
+
+	public final static String TAG = "SODA";
+}
