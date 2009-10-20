2df1897a062cf4f8497544c4773fa78ed34740cd
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2009-10-20 20:02:26 +0000

    submission is being implemented

---
+++ trunk/bankdroid.soda/src/bankdroid/soda/Main.java
@@ -0,0 +1,53 @@
+package bankdroid.soda;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Main extends Activity implements Codes, OnClickListener
+{
+	private static final String HOME_SITE_URL = "http://sites.google.com/site/bankdroidsoda/";
+
+	@Override
+	protected void onCreate( final Bundle savedInstanceState )
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		( (Button) findViewById(R.id.viewLast) ).setOnClickListener(this);
+		( (Button) findViewById(R.id.manageBanks) ).setOnClickListener(this);
+		( (Button) findViewById(R.id.submitSample) ).setOnClickListener(this);
+		( (Button) findViewById(R.id.help) ).setOnClickListener(this);
+		( (Button) findViewById(R.id.about) ).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick( final View v )
+	{
+		//TODO handle viewLast
+		//TODO handle about
+		if ( v.getId() == R.id.manageBanks )
+		{
+			final Intent bankListIntent = new Intent();
+			bankListIntent.setClass(getBaseContext(), BankListActivity.class);
+			startActivity(bankListIntent);
+		}
+		else if ( v.getId() == R.id.help )
+		{
+			final Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse(HOME_SITE_URL));
+			startActivity(viewIntent);
+		}
+		else if ( v.getId() == R.id.submitSample )
+		{
+			final Intent submitIntent = new Intent();
+			submitIntent.setClass(getBaseContext(), SubmitActivity.class);
+			startActivity(submitIntent);
+		}
+	}
+
+}
