836a6d2fe5f42d36ce3c259213f650a134fbe595
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2009-12-08 20:10:31 +0000

    before refactor service

--- trunk/AndroidPortal.hu/src/hu/androidportal/RSSServiceStartReceiver.java
+++ trunk/AndroidPortal.hu/src/hu/androidportal/RSSServiceStartReceiver.java
@@ -3,15 +3,27 @@ package hu.androidportal;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 
 public class RSSServiceStartReceiver extends BroadcastReceiver
 {
 
 	@Override
-	public void onReceive( Context arg0, Intent arg1 )
+	public void onReceive( final Context context, final Intent intent )
 	{
-		// TODO Auto-generated method stub
-
+		if ( Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) )
+		{
+			//FIXME start service on boot
+		}
+		else if ( ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction()) )
+		{
+			final boolean noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+			//FIXME handle connectity change event.
+		}
+		else if ( ConnectivityManager.ACTION_BACKGROUND_DATA_SETTING_CHANGED.equals(intent.getAction()) )
+		{
+			//FIXME handle background data service changed event
+		}
 	}
 
 }
