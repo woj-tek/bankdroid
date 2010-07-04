dc03877b28c36f8c590927eebaff91b6fb8c4260
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2010-07-04 14:01:12 +0000

    additional banks, bank list improvements

--- bankdroid.soda/src/bankdroid/soda/BankProvider.java
+++ bankdroid.soda/src/bankdroid/soda/BankProvider.java
@@ -33,7 +33,7 @@ public class BankProvider extends ContentProvider implements Codes
 	{
 
 		private static final String DATABASE_NAME = "bank.db";
-		private static final int DATABASE_VERSION = 23;//2010-06-20
+		private static final int DATABASE_VERSION = 25;//2010-07-04
 
 		private final Context context;
 
