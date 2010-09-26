88496de26cbb95234a4dd60c56ca90371b8b6c41
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2010-09-26 07:18:03 +0000

    new banks for PL and UA

--- trunk/bankdroid.soda/src/bankdroid/soda/BankProvider.java
+++ trunk/bankdroid.soda/src/bankdroid/soda/BankProvider.java
@@ -33,7 +33,7 @@ public class BankProvider extends ContentProvider implements Codes
 	{
 
 		private static final String DATABASE_NAME = "bank.db";
-		private static final int DATABASE_VERSION = 27;//2010-08-02
+		private static final int DATABASE_VERSION = 28;//2010-09-26
 
 		private final Context context;
 
