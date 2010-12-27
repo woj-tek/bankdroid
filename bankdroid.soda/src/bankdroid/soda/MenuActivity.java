b1ff8606c120a6378a97a27fc6771af9c5b6d47c
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2010-12-27 16:48:05 +0000

    website added to the menu

--- bankdroid.soda/src/bankdroid/soda/MenuActivity.java
+++ bankdroid.soda/src/bankdroid/soda/MenuActivity.java
@@ -2,12 +2,13 @@ package bankdroid.soda;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MenuActivity extends Activity
+public class MenuActivity extends Activity implements Codes
 {
 
 	@Override
@@ -25,6 +26,11 @@ public class MenuActivity extends Activity
 			return true;
 		}
 
+		if ( item.getItemId() == R.id.menuUserGuide )
+		{
+			onOnlineHelp(null);
+			return true;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -34,6 +40,18 @@ public class MenuActivity extends Activity
 		final MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.normalmenu, menu);
 		return true;
+	}
+
+	public void onProductInfo( final View v )
+	{
+		final Intent viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.aboutUrl2)));
+		startActivity(viewIntent);
+	}
+
+	public void onOnlineHelp( final View v )
+	{
+		final Intent viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL_HELP));
+		startActivity(viewIntent);
 	}
 
 	public void onPreferences( final View v )
