2302001428e52ca3bd7f34ebd27a92c8779b5b51
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2011-01-09 11:03:41 +0000

    comment feed fixed, about bkg fixed

--- hu.tminfo/src/bankdroid/rss/EndIDExtractor.java
+++ hu.tminfo/src/bankdroid/rss/EndIDExtractor.java
@@ -3,24 +3,29 @@
  */
 package bankdroid.rss;
 
-final class EndIDExtractor implements IDExtractor
+class EndIDExtractor implements IDExtractor
 {
+	public final static char SEPERATOR = '=';
 	/**
 	 * This is required due to avoid overlap on comments and standard item IDs. This should large enough
 	 * to avoid id duplication.
 	 */
 	private final static long END_ID_SHIFT = 200000000L;
 
-	//http://androidhungary.com/?p=2300
-	@Override
-	public long getId( final CharSequence guid )
+	public long getId( final CharSequence guid, final char separator, final long shift )
 	{
 		final int len = guid.length();
 		for ( int i = len - 1; i > 0; i-- )
 		{
-			if ( guid.charAt(i) == '=' )
-				return END_ID_SHIFT + new Integer(guid.subSequence(i + 1, len).toString());
+			if ( guid.charAt(i) == separator )
+				return shift + new Integer(guid.subSequence(i + 1, len).toString());
 		}
 		throw new IllegalArgumentException("Invalid guid value for EndIDExctractor: " + guid);
+	}
+
+	@Override
+	public long getId( final CharSequence guid )
+	{
+		return getId(guid, SEPERATOR, END_ID_SHIFT);
 	}
 }
