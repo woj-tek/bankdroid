48e47f63df47e85e72cf647d38c6a9097d3a029f
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2011-01-15 22:35:41 +0000

    SMSKey v2.1

--- trunk/bankdroid.soda/src/bankdroid/soda/BankProvider.java
+++ trunk/bankdroid.soda/src/bankdroid/soda/BankProvider.java
@@ -33,7 +33,7 @@ public class BankProvider extends ContentProvider implements Codes
 	{
 
 		private static final String DATABASE_NAME = "bank.db";
-		private static final int DATABASE_VERSION = 34;//2011-01-02
+		private static final int DATABASE_VERSION = 35;//2011-01-15
 
 		private final Context context;
 
