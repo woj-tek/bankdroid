70094fe26d7541c7fea75580c37f466296f81157
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2011-04-14 17:10:01 +0000

    CIMB (SG) added, v2.7.1 finalized

--- bankdroid.soda/src/bankdroid/soda/BankProvider.java
+++ bankdroid.soda/src/bankdroid/soda/BankProvider.java
@@ -36,7 +36,7 @@ public class BankProvider extends ContentProvider implements Codes
 	{
 
 		private static final String DATABASE_NAME = "bank.db";
-		private static final int DATABASE_VERSION = 43;//2011-04-13
+		private static final int DATABASE_VERSION = 44;//2011-04-14
 
 		DatabaseHelper( final Context context )
 		{
