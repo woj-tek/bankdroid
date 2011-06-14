1cce0901b68891c3322c6ed50211cfb049f0e436
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2011-06-14 19:21:22 +0000

    v2.10

--- trunk/bankdroid.soda/src/bankdroid/soda/BankProvider.java
+++ trunk/bankdroid.soda/src/bankdroid/soda/BankProvider.java
@@ -36,7 +36,7 @@ public class BankProvider extends ContentProvider implements Codes
 	{
 
 		private static final String DATABASE_NAME = "bank.db";
-		private static final int DATABASE_VERSION = 48;//2011-05-24
+		private static final int DATABASE_VERSION = 49;//2011-06-14
 
 		DatabaseHelper( final Context context )
 		{
