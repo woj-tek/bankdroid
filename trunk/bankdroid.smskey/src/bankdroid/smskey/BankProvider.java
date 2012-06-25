a60ce34fa584aa778972f382ef6c85208af31570
gyenesge <gyenesge@c6093680-996e-11de-befb-3d8cfe884659>
2012-06-25 22:38:12 +0000

    v3.10

--- trunk/bankdroid.smskey/src/bankdroid/smskey/BankProvider.java
+++ trunk/bankdroid.smskey/src/bankdroid/smskey/BankProvider.java
@@ -37,7 +37,7 @@ public class BankProvider extends ContentProvider implements Codes
 	{
 
 		private static final String DATABASE_NAME = "bank.db";
-		private static final int DATABASE_VERSION = 12;//2012-04-21
+		private static final int DATABASE_VERSION = 13;//2012-06-26
 
 		private final Context context;
 
